1d0209a133e12158a6081be1982bd4f54d707e93
Lian Randle <liantrandle@gmail.com>
2018-06-14 10:21:48 -0400

    Add files via upload

---
+++ TipsApp/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  TipsApp
+//
+//  Created by Lian Randle on 6/13/18.
+//  Copyright © 2018 KBLR. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
